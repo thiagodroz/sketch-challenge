356f8b3b75ee499fbb4410b306514406be465688
Thiago Nogueira <thiago.droz@gmail.com>
2022-02-23 10:43:45 -0300

    Setting up styled-components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,9 +2,11 @@ import { VFC } from 'react'
 import { ApolloProvider } from '@apollo/client'
 
 import { client } from 'graphql/client'
+import { GlobalStyles } from 'styles/global'
 
 const App: VFC = () => (
   <ApolloProvider client={client}>
+    <GlobalStyles />
     <div>Sketch Challenge</div>
   </ApolloProvider>
 )
