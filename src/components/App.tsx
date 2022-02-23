b0f15292578721fcef7dcdd214e967352c1dd579
Thiago Nogueira <thiago.droz@gmail.com>
2022-02-23 11:08:02 -0300

    Setting up reate-router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,22 @@
 import { VFC } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/client'
 
 import { client } from 'graphql/client'
 import { GlobalStyles } from 'styles/global'
 
 const App: VFC = () => (
-  <ApolloProvider client={client}>
-    <GlobalStyles />
-    <div>Sketch Challenge</div>
-  </ApolloProvider>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<div>home</div>} />
+        <Route path="/documents/:documentId" element={<div>document</div>} />
+        <Route path="/artboards/:artboardId" element={<div>artboard</div>} />
+        <Route path="*" element={<div>Not found</div>} />
+      </Routes>
+    </ApolloProvider>
+  </BrowserRouter>
 )
 
 export default App
