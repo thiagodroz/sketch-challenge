c8691389eba3baaeed41ff8687f612e013d59780
Thiago Nogueira <thiago.droz@gmail.com>
2022-02-26 23:06:16 -0300

    Adding Header stories

---
+++ src/components/shared/Header/Header.stories.tsx
@@ -0,0 +1,43 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import styled from 'styled-components'
+
+import { Header } from './Header'
+
+export default {
+  title: 'Shared/Header',
+  component: Header,
+  argTypes: {},
+} as ComponentMeta<typeof Header>
+
+const Template: ComponentStory<typeof Header> = args => <Header {...args} />
+
+export const StringChildren = Template.bind({})
+StringChildren.args = {
+  children: 'Header component',
+  onCloseRedirectPath: '',
+}
+
+const CustomHeaderContent = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const ReactNodeChildren = Template.bind({})
+ReactNodeChildren.args = {
+  children: (
+    <CustomHeaderContent>
+      <div>Left</div>
+      <div>Middle</div>
+      <div>Right</div>
+    </CustomHeaderContent>
+  ),
+  onCloseRedirectPath: '',
+}
+
+export const WithCloseButton = Template.bind({})
+WithCloseButton.args = {
+  children: 'Header component',
+  onCloseRedirectPath: '/',
+}
